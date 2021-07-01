cbc44fe99e68a7b87f84078ce9e756379a815d1e
Mike Edwards <michael.edwards.4108@gmail.com>
2021-07-01 12:25:54 -0400

    Created Collections and Cards class in models.py.

--- flashcards/flash_cards/models.py
+++ flashcards/flash_cards/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Collections(models.Model):
+    collection_id = models.IntegerField(primary_key=True)
+    collection_name = models.CharField(max_length=25)
+
+
+class Cards(models.Model):
+    card_id = models.IntegerField(primary_key=True)
+    key_word = models.CharField(max_length=50)
+    definition = models.CharField(max_length=150)
+    collection_id = models.ForeignKey(Collections, blank=True, null=True, on_delete=models.CASCADE)
