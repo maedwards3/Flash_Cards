358c4b9352ae5f616b8077d3e32bcabc791a5c98
Mike Edwards <michael.edwards.4108@gmail.com>
2021-07-06 15:49:31 -0400

    Started home page component to get collections from database.

---
+++ flashcard_app/src/Components/HomePage/homePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const HomePage = (props) => {
+
+    return (
+        <div className="container overflow-hidden">
+            <div className="row gy-5">
+                {props.collections.map(collection => {
+                    return (
+                        <div className="col-3">
+                            <div className="p-3 border bg-light">{collection.collection_name}</div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    )
+}
+    
+export default HomePage;
