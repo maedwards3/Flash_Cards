528b2064f8544170537731b5565fef93abc1ae93
Mike Edwards <michael.edwards.4108@gmail.com>
2021-07-06 15:53:21 -0400

    Installed axios and bootstrap to flashcard_app. Including index.js creation in commit.

---
+++ flashcard_app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './Components/app';
+import 'bootstrap/dist/css/bootstrap.css';
+
+ReactDom.render(<App />, document.getElementById('root'));
