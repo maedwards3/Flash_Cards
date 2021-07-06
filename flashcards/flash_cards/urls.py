4379089393d578b59007fd9a3094ce84d14bfbb5
Mike Edwards <michael.edwards.4108@gmail.com>
2021-07-06 10:44:19 -0400

    Refactored Card class urls. Created CardDetail views methods to complete CRUD. Started frontend react app.

--- flashcards/flash_cards/urls.py
+++ flashcards/flash_cards/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('collection/', views.CollectionsList.as_view()),
     path('collectionDetail/<int:collection_id>/', views.CollectionsDetail.as_view()),
-    path('card/', views.CardsList.as_view())
+    path('collection/<int:collection_id>/card/', views.CardsList.as_view()),
+    path('collection/<int:collection_id>/cardDetail/<int:card_id>/', views.CardsDetail.as_view())
 ]
--- flashcards/flash_cards/views.py
+++ flashcards/flash_cards/views.py
@@ -51,16 +51,41 @@ class CollectionsDetail(APIView):
 
 
 class CardsList(APIView):
-
-    def get(self, request):
-        card = Cards.objects.all()
-        serializer = CardsSerializer(card, many=True)
+    def get(self, request, collection_id):
+        cards = Cards.objects.filter(collection_id=collection_id).all()
+        serializer = CardsSerializer(cards, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
+    def post(self, request, collection_id):
         serializer = CardsSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class CardsDetail(APIView):
+
+    def get_card(self, card_id):
+        try:
+            return Cards.objects.get(card_id=card_id)
+        except Cards.DoesNotExist:
+            raise Http404
+
+    def get(self, request, collection_id, card_id):
+        card = self.get_card(card_id)
+        serializer = CardsSerializer(card)
+        return Response(serializer.data)
+
+    def put(self, request, collection_id, card_id):
+        card = self.get_card(card_id)
+        serializer = CardsSerializer(card, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, collection_id, card_id):
+        card = self.get_card(card_id)
+        card.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
