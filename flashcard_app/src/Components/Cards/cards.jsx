7fddab7c184142767c3b8b93f9efe2fc59cb2da0
Mike Edwards <michael.edwards.4108@gmail.com>
2021-07-08 18:46:16 -0400

    Refactored to show a front and back of card with the help of a button click.

--- flashcard_app/src/Components/Cards/cards.jsx
+++ flashcard_app/src/Components/Cards/cards.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 
 const Cards = (props) => {
-    const [cardNumber, setCardNumber] = useState(0)
+    const [cardNumber, setCardNumber] = useState(0);
+    const [sideShown, setSideShown] = useState('front');
+    const selectedCard = props.cards[cardNumber];
+
 
     function previousCard() {
-        let card = cardNumber;
-        card--;
-        if(card < 0) {
+        let card = cardNumber - 1;
+        if (card < 0) {
             card = props.cards.length - 1;
         }
         setCardNumber(card)
@@ -14,31 +16,42 @@ const Cards = (props) => {
     }
 
     function nextCard() {
-        let card = cardNumber;
-        card++;
-        if(card === props.cards.length) {
+        let card = cardNumber + 1;
+        if (card === props.cards.length) {
             card = 0;
         }
         setCardNumber(card)
         console.log("next button is being clicked") //test
     }
 
+    const onShowBack = () => {
+        setSideShown('back');
+    }
+    
+    const onShowFront = () => {
+        setSideShown('front');
+    }
+
     return (
-        <div>
-            {props.cards.map(card => {
-                console.log(card)
-                return(
-                    <div className="card">
-                        <div className="card-body">
-                            <p>{cardNumber} of {props.cards.length}</p>
-                            <h4 className="card-title">{card.key_word}</h4>
-                            <p className="card-text">{card.definition}</p>
-                            <button onClick={() => previousCard()}>Previous</button>
-                            <button onClick={() => nextCard()}>Next</button>
-                        </div>
+        <div className="card">
+            <div>{console.log("Cards component props.cards", props.cards)}</div>
+            <div className="card-body back">
+                <p>{cardNumber + 1} of {props.cards.length + 1}</p>
+                {
+                    sideShown === 'front' ?
+                    <div className="card-content front">
+                        <h4 className="card-title">{selectedCard.key_word}</h4>
+                        <button onClick={onShowBack}>Flip to back</button>
                     </div>
-                )
-            })}
+                    :
+                    <div className="card-content back">
+                        <h5 className="card-text">{selectedCard.definition}</h5>
+                        <button onClick={onShowFront}>Flip to front</button>
+                    </div>
+                }
+                <button onClick={() => previousCard()}>Previous</button>
+                <button onClick={() => nextCard()}>Next</button>
+            </div>
         </div>
     )
 }
