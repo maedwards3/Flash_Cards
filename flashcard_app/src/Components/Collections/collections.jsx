7e9387d9d1c7dadc1124bd615d99d07822cd7c08
Mike Edwards <michael.edwards.4108@gmail.com>
2021-07-08 18:44:30 -0400

    Refactored to display collections only when flashcards are not displayed.

--- flashcard_app/src/Components/Collections/collections.jsx
+++ flashcard_app/src/Components/Collections/collections.jsx
@@ -1,28 +1,49 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import Cards from '../Cards/cards';
 import './collections.css'
 
-const Collection = (props) => {
+const Collection = ({ selectedCollection, setSelectedCollection }) => {
     const [collection, setCollection] = useState([]);
 
     useEffect(() => {
-        axios.get('http://127.0.0.1:8000/collection/')
-        .then(response => setCollection(response.data))
-    }, [])
-    ;
+            axios.get('http://127.0.0.1:8000/collection/')
+            .then(response => setCollection(response.data))
+    }, []);
 
     return (
         <div className="container overflow-hidden">
             <div className="row gy-5">
-                {collection.map(collection => {
-                    console.log(collection)
-                    return (
-                        <div className="col-md-3 collectionBox">
-                            <div type="button" className="p-3 border bg-light" onClick={() => props.selectedCollection(collection.collection_id)}>{collection.collection_name}</div>
+                {
+                    selectedCollection
+                    ?
+                    <button onClick={() => setSelectedCollection(null)}>Return to Collection Selection</button>
+                    :
+                    // collection.map(item => {
+                    //     console.log(item)
+                    //     return (
+                    //         <div key={item.collection_id} className="col-md-3 collectionBox">
+                    //             <div type="button" className="p-3 border bg-light" onClick={() => setSelectedCollection(item.collection_id)}>{item.collection_name}</div>
+                    //         </div>
+                    //     );
+                    // })
+                    <>
+                        <div className="container-fluid">
+                            <div className="row">
+                                <div className="col-md-1"></div>
+                                <div className="col-md-3 directions"><h5>Select a stack to study</h5></div>
+                                <div className="col-md-8"></div>
+                            </div>
                         </div>
-                    );
-                })}
+                        {collection.map(item => {
+                            console.log(item)
+                            return (
+                                <div key={item.collection_id} className="col-md-3 collectionBox">
+                                    <div type="button" className="p-3 border bg-light" onClick={() => setSelectedCollection(item.collection_id)}>{item.collection_name}</div>
+                                </div>
+                            );
+                        })}
+                    </>
+                }
             </div>
         </div>
     )
